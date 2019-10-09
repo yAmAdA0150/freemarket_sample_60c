438971f15813f956699482198e02ca40d3868188
ashurisj <new4s9_asm97@icloud.com>
2019-10-09 12:46:12 +0900

    items_controller_and_routes.rb_change
    アイテムコントローラーとルートを編集しました。

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,25 @@
 class ItemsController < ApplicationController
-
+  
   def index
   end 
+
+  def show
+  end
+
+  def edit
+  end
+
+  def destroy
+  end
+
+  def create
+  end
+
+  def update
+  end
+
+  def new
+  end
+
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do           
   devise_for :users
 
-  root to: 'items#index'
+  # root to: 'items#index'
 
 
   # devise_scope :user do
@@ -17,7 +17,8 @@ Rails.application.routes.draw do
   # end
 
 
-  
+  resources :items do
+  end
 
   resources :signup ,only: [:index, :create] do
     collection do
