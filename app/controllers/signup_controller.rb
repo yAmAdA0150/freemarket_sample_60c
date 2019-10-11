81fcc2557e9108397a6a3a3816c95017a060b4e8
yamadamaya513 <yunaray513@gmail.com>
2019-10-11 13:10:13 +0900

    edit_view

--- app/controllers/signup_controller.rb
+++ app/controllers/signup_controller.rb
@@ -4,36 +4,21 @@ class SignupController < ApplicationController
   end
 
   def registration
-    # session[:name] = user_params[:name]
-    # session[:email] = user_params[:email]
-    # session[:password] = user_params[:password]
-    # session[:password_confirmation] = user_params[:password_confirmation]
-    # session[:first_name] = user_params[:first_name]
-    # session[:last_name] = user_params[:last_name]
-    # session[:first_name_kana] = user_params[:first_name_kana]
-    # session[:last_name_kana] = user_params[:last_name_kana]
-    # session[:birthyear] = user_params[:birthyear]
-    # session[:birthmonth] = user_params[:birthmonth]
-    # session[:birthday] = user_params[:birthday]
     @user = User.new
   end
 
 
   def sms_confimation
-    # session[:phone_number] = profile_params[:phone_number]
-    # @user.build_profiles
     @user = User.new
   end
 
   def address
     @prefectures = Prefecture.all
     @user = User.new
-    # @user.build_address
-  end
+s  end
 
   def creditcard
     @user = User.new
-
   end
 
   def complete
