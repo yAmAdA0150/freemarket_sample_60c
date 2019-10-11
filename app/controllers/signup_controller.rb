010ff7b64bce572566d5de4c886db96a631a153f
yamadamaya513 <yunaray513@gmail.com>
2019-10-11 12:35:19 +0900

    sign_up_views

--- app/controllers/signup_controller.rb
+++ app/controllers/signup_controller.rb
@@ -1,21 +1,20 @@
 class SignupController < ApplicationController
 
   def index
-    @user = User.new
   end
 
   def registration
-    session[:name] = user_params[:name]
-    session[:email] = user_params[:email]
-    session[:password] = user_params[:password]
-    session[:password_confirmation] = user_params[:password_confirmation]
-    session[:first_name] = user_params[:first_name]
-    session[:last_name] = user_params[:last_name]
-    session[:first_name_kana] = user_params[:first_name_kana]
-    session[:last_name_kana] = user_params[:last_name_kana]
-    session[:birthyear] = user_params[:birthyear]
-    session[:birthmonth] = user_params[:birthmonth]
-    session[:birthday] = user_params[:birthday]
+    # session[:name] = user_params[:name]
+    # session[:email] = user_params[:email]
+    # session[:password] = user_params[:password]
+    # session[:password_confirmation] = user_params[:password_confirmation]
+    # session[:first_name] = user_params[:first_name]
+    # session[:last_name] = user_params[:last_name]
+    # session[:first_name_kana] = user_params[:first_name_kana]
+    # session[:last_name_kana] = user_params[:last_name_kana]
+    # session[:birthyear] = user_params[:birthyear]
+    # session[:birthmonth] = user_params[:birthmonth]
+    # session[:birthday] = user_params[:birthday]
     @user = User.new
   end
 
@@ -23,15 +22,18 @@ class SignupController < ApplicationController
   def sms_confimation
     # session[:phone_number] = profile_params[:phone_number]
     # @user.build_profiles
-    # @user = User.new
+    @user = User.new
   end
 
   def address
+    @prefectures = Prefecture.all
     @user = User.new
-    @user.build_address
+    # @user.build_address
   end
 
   def creditcard
+    @user = User.new
+
   end
 
   def complete
