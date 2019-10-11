12651e0a7b085e7ab1b0ca1e46f54f6fa75a6bec
yAmAdA0150 <omnis9999@gmail.com>
2019-10-11 20:46:37 +0900

    add card infomation to routes,controller,application

--- config/routes.rb
+++ config/routes.rb
@@ -27,6 +27,15 @@ Rails.application.routes.draw do
     resources :addresses, only: [:update, :edit]
     
   # end
+
+  # resources :card, only: [:new, :show] do
+  #   collection do
+  #     post 'show', to: 'card#show'
+  #     post 'pay', to: 'card#pay'
+  #     post 'delete', to: 'card#delete'
+  #   end
+  # end
+
   
   resources :items do
     collection do
