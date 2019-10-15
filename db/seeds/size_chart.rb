928c218e56179ecd5bef72048a06875885f14de4
ShoSeshake <sho122seshake@gmail.com>
2019-10-15 18:23:45 +0900

    seed size chart

---
+++ db/seeds/size_chart.rb
@@ -0,0 +1,96 @@
+for size in 1..10 do 
+  for category in 159..218 do 
+    SizeChart.create({size_id:size,category_id:category})
+  end
+  for category in 229..230 do 
+    SizeChart.create({size_id:size,category_id:category})
+  end
+  for category in 315..321 do 
+    SizeChart.create({size_id:size,category_id:category})
+  end
+  for category in 326..333 do 
+    SizeChart.create({size_id:size,category_id:category})
+  end
+  for category in 338..381 do 
+    SizeChart.create({size_id:size,category_id:category})
+  end
+  for category in 402..405 do 
+    SizeChart.create({size_id:size,category_id:category})
+  end
+  for category in 457..458 do 
+    SizeChart.create({size_id:size,category_id:category})
+  end
+  for category in 1088..1089 do 
+    SizeChart.create({size_id:size,category_id:category})
+  end
+  for category in 1099..1100 do 
+    SizeChart.create({size_id:size,category_id:category})
+  end
+end
+
+for size in 11..26 do 
+  for category in 219..228 do 
+    SizeChart.create({size_id:size,category_id:category})
+  end 
+    SizeChart.create({size_id:size,category_id:1086})
+    SizeChart.create({size_id:size,category_id:1096})
+end
+
+for size in 27..42 do 
+  for category in 382..389 do 
+    SizeChart.create({size_id:size,category_id:category})
+  end
+  SizeChart.create({size_id:size,category_id:1085})
+  SizeChart.create({size_id:size,category_id:1095})
+end
+
+for size in 55..62 do 
+  for category in 536..540 do 
+    SizeChart.create({size_id:size,category_id:category})
+  end
+  SizeChart.create({size_id:size,category_id:1087})
+  SizeChart.create({size_id:size,category_id:1097})
+end
+
+for size in 48..54 do 
+  for category in 496..535 do 
+    SizeChart.create({size_id:size,category_id:category})
+  end
+  SizeChart.create({size_id:size,category_id:1090})
+  SizeChart.create({size_id:size,category_id:1101})
+end
+
+for size in 124..130 do 
+    SizeChart.create({size_id:size,category_id:1083})
+end
+
+for size in 103..116 do 
+  for category in 1221..1224 do 
+    SizeChart.create({size_id:size,category_id:category})
+  end
+end
+
+for size in 63..67 do 
+  for category in 469..495 do 
+    SizeChart.create({size_id:size,category_id:category})
+  end
+end
+
+for size in 78..89 do 
+  for category in 979..980 do 
+    SizeChart.create({size_id:size,category_id:category})
+  end
+end
+
+for size in 68..77 do 
+  SizeChart.create({size_id:size,category_id:984})
+end
+for size in 117..123 do 
+  SizeChart.create({size_id:size,category_id:1083})
+end
+for size in 96..102 do 
+  SizeChart.create({size_id:size,category_id:1268})
+end
+for size in 90..95 do 
+  SizeChart.create({size_id:size,category_id:147})
+end
