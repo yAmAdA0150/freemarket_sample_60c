9ec49cb64a8b68d1534c5cafb5cf047b6b207efe
ShoSeshake <sho122seshake@gmail.com>
2019-10-19 13:08:12 +0900

    implement category function

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,9 +1,25 @@
 class CategoriesController < ApplicationController
   def index
-    categories = Category.all
+    @categories = Category.all
+    @parents = Category.all.where(ancestry: nil)
   end
 
   def show
     @category = Category.find(params[:id])
+    if @category.has_children?
+      @items = Item.all.where(category_id: @category.descendant_ids)
+    else
+      @items = @category.items
+    end
+    # if @category.ancestry == nil
+    #   @children = @category.children
+    #   @items = []
+      
+    # elsif @cateogry.ancestry =< 13
+    #   @children = @category.children
+    #   @items = 
+    # else
+    #   @siblings = Category.all.where(ancestry: @category.ancestry)
+    # end
   end
 end
