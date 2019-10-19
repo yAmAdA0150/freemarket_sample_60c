3521e797f423a57c3a5a627c4e089e4ae3716e1c
ShoSeshake <sho122seshake@gmail.com>
2019-10-19 11:34:02 +0900

    modiified image uploading

--- app/models/image.rb
+++ app/models/image.rb
@@ -2,6 +2,6 @@ class Image < ApplicationRecord
   belongs_to :item, dependent: :destroy 
   mount_uploader :url, ImageUploader
 
-  validates :image, presence: true
+  validates :url, presence: true
   validates :item, presence: true
 end
