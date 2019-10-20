fb2977ce91a94f51ff81bb9dbbdc1bf0881d0d54
ShoSeshake <sho122seshake@gmail.com>
2019-10-20 15:12:03 +0900

    modified route

--- config/routes.rb
+++ config/routes.rb
@@ -3,72 +3,36 @@ Rails.application.routes.draw do
   root to: 'items#index'
 
   # resource :user, only: [:destroy]
-  # まだテーブルにレコードがないが、ビューの作成のためにコメントアウトしてあります
 
-  # resources :items, only: [:index, :show,] do
-  #   resources :users do
-  #     member do
-  #       get 'buy'
-  #     end
-  #   end
-  # end
   resources :categories, only: [:show, :index]
-  resources :brands, only:[:show ,:index]
-  
-  
+
   resources :users, only: [:show, :edit, :destroy, :update] do
-    collection do
-      get 'profile'
+    member do
       get 'address'
       get 'signout'
     end
-    resource :cards, only: [:new, :show, :create, :edit, :update, :destroy] do
+    resource :cards, only: [:new, :show] do
       collection do
-
         get 'create'
         get 'payment' => 'cards#payment'
         get 'delete'
       end
     end
-    resource :address, only: [:edit, :update]
-
+    resources :items, only: [:new,:create, :edit,:update] do
+      collection do
+        get 'buy'
+      end
+    end
   end
-  
-  
-  
-  # resources :users do
-  #   resources :items , only:[:edit, :update, :destroy, :new ,:create] do
-  #     collection do
-  #       get 'search'
-  #     end
-  #   end
-    
-    
 
-    
-    resources :addresses, only: [:update, :edit]
-    
-  # end
-
-  # resources :card, only: [:new, :show] do
-  #   collection do
-  #     post 'show', to: 'card#show'
-  #     post 'pay', to: 'card#pay'
-  #     post 'delete', to: 'card#delete'
-  #   end
-  # end
+  resources :items, only: [:index, :show]do
+    collection do
+      get 'search'
+    end
+  end
 
   namespace :api do
     resources :categories, only: :index
-  end
-
-  
-  resources :items, only: [:new, :create, :show] do
-    collection do
-      get 'buy'
-      get 'edit'
-      get 'search'
-    end
   end
 
   resources :signup do 
