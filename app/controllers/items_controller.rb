95abed26e865b73cdeb2797b469ae5310027fdc5
yamadamaya513 <yunaray513@gmail.com>
2019-10-21 20:16:24 +0900

    search_edit

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -37,6 +37,7 @@ class ItemsController < ApplicationController
 
   def search
     @items = Item.where('name LIKE(?)',"%#{params[:keyword]}%").limit(20)
+
   end
 
   def edit
--- spec/controllers/items_controller_spec.rb
+++ spec/controllers/items_controller_spec.rb
@@ -9,5 +9,32 @@ RSpec.describe ItemsController, type: :controller do
       expect(response).to render_template :index
     end
   end
-end
+    describe "GET #search" do
+    it "renders the :search template" do
+      get :search
+      expect(@respnse).to render_template :search
+    end
+  end
+  #   describe "GET #new" do
+  #   # session = {action:"new",controller:"items"}
+  #   it "renders the :new template" do
+  #     get :new
+  #     expect(respnse).to redirect_to(new_items_path)
+  #   end
+  # end
+  # describe "GET #show" do
+  # # session = {action: "show",controller: "items"}
+  # it "renders the :show template" do
+  #   get :show
+  #   expect(respnse).to render_template :show
+  #   end
+  # end
+  # describe "GET #create" do
+  # # session = {action: "show",controller: "items"}
+  # it "renders the :create template" do
+  #   get :create
+  #   expect(respnse).to render_template :create
+  #   end
+  # end
 
+end
