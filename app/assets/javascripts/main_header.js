d2882eee52fea6b0fd1258a4db45b7c5a7cc2338
ashurisj <new4s9_asm97@icloud.com>
2019-10-21 17:30:23 +0900

    header_category
    ヘッダーのカテゴリー一覧をjsと非同期で記述しました。

--- app/assets/javascripts/main_header.js
+++ app/assets/javascripts/main_header.js
@@ -1,117 +1,85 @@
 $(function () {
 
   var categorySearch = $("#search");
-  categorySearch.on("mouseover", function () {
+  categorySearch.on("mouseenter", function () {
     $(".all-category").css({
       'display': `block`
     });
     $("#left").css({
       'display': `block`
     });
-      // (function () {
-      //   $(".all-category", "#left").css({
-      //     'display': 'none'
-      //   })
-      // });
-    
+    $("#center").css({
+      'display': 'none'
+    });
+    $("#right").css({
+      'display': 'none'
+    });
+  });
+  $("#search").on("mouseleave", function () {
+    $(".all-category").css({
+      'display': 'none'
+    });
   });
 
-  var categoryParent = $("#center");
-  // var liCategory = $('.all-category__first__type');
-  var categoryAll = $('.all-category__first__type')
-  categoryAll.on("mouseover", function () {
-      var parent = $(this).val();
-      // debugger
-      $.ajax({
-        type: 'GET',
-        url: '/api/categories',
-        data: { parent: parent },
-        dataType: 'json'
-      })
-        .done(function (children) {
-          var childCategory = $('#center')
-          childCategory.css({
-            'display': `block`
-          });
-          children.forEach(function (child) {
-            var selectChild = $(`<li class="all-category__second__menu" value="${child.ancestry}/${child.id}">${child.name}</li>`);
-            categoryParent.append(selectChild)
-          });
+
+  $(".all-category__first__type").on("mouseenter", function () {
+    var parent = $(this).attr('value');
+    $.ajax({
+      type: 'GET',
+      url: '/api/categories',
+      data: { parent: parent },
+      dataType: 'json'
+    })
+      .done(function (children) {
+        var childCategory = $('#center');
+        childCategory.css({
+          'display': `block`
         });
- 
+
+        function appendChild(child) {
+          var html = `<a href="/categories/child.id", class="link"><option value="${child.ancestry}/${child.id}", class="category-child" >${child.name}</option></a>`
+          childCategory.append(html);
+        }
+        if (children.length !== 0) {
+          childCategory.empty();
+          children.forEach(function (child) {
+            appendChild(child);
+          });
+        };
+      });
+  });
+  $(".all-category__first__type").on("mouseenter", function () {
+    $("#right").css({
+      'display': 'none'
+    });
   });
 
   $(function () {
-    var categoryChild = $("#third")
-    var listChild = $("#second")
-    listChild.on("mouseover", function () {
-      var child = $(this).val();
+    $(document).on("mouseenter", ".category-child", function () {
+      var child = $(this).attr('value');
       $.ajax({
         type: 'GET',
         url: '/api/categories',
         data: { child: child },
         dataType: 'json'
       })
-        .done(function (children) {
-          var gchildCategory = ('#right')
+        .done(function (gchildren) {
+          var gchildCategory = $('#right');
           gchildCategory.css({
-            'display': `block`
-          })
-          children.forEach(function (child) {
-          var selectGchild = $(`<li class="all-category__third__list" value="${gchild.ancestry}/${gchild.id}">${gchild.name}</li>`);
-          categoryChild.append(selectGchild)
+            'display': `block`,
           });
-        });
-    });
-  });
 
-  $(function () {
-    var listGchild = $("right")
-    listGchild.on("mouseremove", function () {
-      var gchild = $(this).val();
-      $.ajax({
-        type: 'GET',
-        url: '/api/categories',
-        data: { gchild: gchild },
-        dataType: 'json'
-      })
-        .done(function () {
-          var allList = $('.all-category')
-          allList.css({
-            'display': 'block'
-          });
+          function appendGchild(gchild) {
+            var html = `<a href="/categories/gchild.id", class="link"><option value="${gchild.id}", class="category-gchild">${gchild.name}</option></a>`
+            gchildCategory.append(html);
+          }
+          if (gchildren.length !== 0) {
+            gchildCategory.empty();
+            gchildren.forEach(function (gchild) {
+              appendGchild(gchild);
+            });
+          };
         });
     });
   });
 });
-
-
-
-// $(function () {
-//   $('#search').hover(function () {   //htmlで指定したid:searchにカーソルを合わせた際の動き
-//     $($(this).children('#left'))     //idがleftを指定
-//       .css('display', 'block')       //displayをnoneからblockへ変更
-//   },
-//     function () {                    //上のhover以外の動作をした時と指定
-//       $($(this).children('#left'))   //idがleftを指定
-//         .css('display', 'none')      //didplayをnoneへ変更
-//     });
-  
-//   $('#parent').hover(function () {    //htmlで指定したid:parentにカーソルを合わせた際の動き
-//     $($(this).children('#center'))    //id:centerを指定
-//       .css('display', 'block')        //displayをnoneからblockへ変更
-//   },
-//     function () {                     //上のhover以外の動作をした時と指定
-//       $($(this).children('#center'))  //id:centerを指定
-//         .css('display', 'none')       //didplayをnoneへ変更
-//     });
-  
-//   $('#child').hover(function () {     //htmlで指定したid:childにカーソルを合わせた際の動き
-//     $($(this).children('#right'))     //id:rightrを指定
-//       .css('display', 'block')        //displayをnoneからblockへ変更
-//   },
-//     function () {                     //上のhover以外の動作をした時と指定
-//       $($(this).children('#right'))   //id:rightを指定
-//         .css('display', 'none')       //didplayをnoneへ変更
-//     });
-// });
