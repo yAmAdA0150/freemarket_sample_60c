b7d433ea366c17cc51356bf3a951157363278446
ShoSeshake <sho122seshake@gmail.com>
2019-10-23 14:37:46 +0900

    implement smooth scroll

---
+++ app/assets/javascripts/scroll.js
@@ -0,0 +1,10 @@
+$(function() {
+    $('a[href^="#"]').click(function() {
+        var href = $(this).attr("href");
+        var target = $(href == "#" || href == "" ? 'html' : href);
+        var position = target.offset().top;
+        var speed = 500; // スクロールの速度（ミリ秒）
+        $('body,html').animate({ scrollTop: position }, speed, 'swing'); // スクロール方式
+        return false;
+    });
+});
