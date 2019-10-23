e70bae3254c689f97d8fe0a7e9b7dee86297a46d
ashurisj <new4s9_asm97@icloud.com>
2019-10-23 12:42:11 +0900

    item.controller.add
    ・購入完了ページのためにdone部分を追記しました。

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -109,10 +109,23 @@ class ItemsController < ApplicationController
     )
     @trading.save
 
+    @item.update(display_id:4)
+
     redirect_to action: "done"
   end
 
   def done
+    @item = Item.find(params[:id])
+    @image= Image.find(@item.id)
+    card = Card.where(user_id: current_user.id).first
+      if card.blank?
+        redirect_to controller: "card", action: "new"
+      else
+        Payjp.api_key = ENV["PAYJP_PRIVATE_KEY"]
+        customer = Payjp::Customer.retrieve(card.customer_id)
+        @default_card_information = customer.cards.retrieve(card.card_id)
+      end
+    @address = Address.find(params[:user_id])
   end
 
   private
