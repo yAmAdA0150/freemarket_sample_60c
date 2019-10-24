c016f11f06b1e8ac931b9fd4ac709eb428187705
mika631 <kikijiji336@gmail.com>
2019-10-24 11:07:39 +0900

    confirmation_remake
    カード登録情報がない人が購入する場合を修正しました

--- config/routes.rb
+++ config/routes.rb
@@ -22,7 +22,7 @@ Rails.application.routes.draw do
       member do
         get 'done'
         get 'confirmation'
-        post 'confirmation' => 'items#buy'
+        post 'buy'
       end
     end
   end
