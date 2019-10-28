3d04ab8814bf3ea6ab87a9d520986dce7e730b2e
ShoSeshake <sho122seshake@gmail.com>
2019-10-28 14:02:11 +0900

    deleted files

--- app/models/Sort.rb
+++
@@ -1,6 +0,0 @@
-class Sort < ActiveHash::Base
-  self.data = [
-      {id: 1, name: '並び替え'}, {id: 2, name: '価格の安い順'}, {id: 3, name: '価格の高い順'},
-      {id: 4, name: '出品の古い順'}, {id: 5, name: '出品の新しい順'}, {id: 6, name: 'いいね！の多い順'}
-  ]
-end
